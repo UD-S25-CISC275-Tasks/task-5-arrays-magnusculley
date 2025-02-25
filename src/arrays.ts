84da37070618c62d9fa2578f0162f9de778a10d0
magnus <magnus@udel.edu>
2025-02-24 22:48:44 -0500

    tripling numbers using map for tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -19,7 +19,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((price: number): number => price * 3);
+    return tripled;
 }
 
 /**
